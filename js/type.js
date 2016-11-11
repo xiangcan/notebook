3356f98ab4e3f38842b30a4336b8137b984c8741
xiangcan <can.heung@gmail.com>
2016-11-11 22:38:39 +0800

    Update type.js

--- js/type.js
+++ js/type.js
@@ -31,7 +31,7 @@ define(['jquery'],function($) {
 
                 var timer = setInterval(function(){
                     //substr(index, 1) 方法在字符串中抽取从index下标开始的一个的字符
-                    var current = str.splice(index, 1);
+                    var current = str.substr(index, 1);
 
                     if(current == '<'){
                     //indexOf() 方法返回">"在字符串中首次出现的位置。
